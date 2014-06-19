bb4f853de6ea37bf54cdf9af28b5d33eda1c7398
David Wu <dwu7401@gmail.com>
2014-06-18 23:22:24 -0700

    Better parsing. Bugs fixed

--- sshkeysetup.sh
+++ sshkeysetup.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
-    IP=$1
-else
-    echo "Enter remote IP to setup"
+if [[ "$#" < 1 || "$#" > 2 ]]; then
+    echo "Enter Username and IP in the form:"
+    echo './sshkeysetup.sh "user@ip"'
+    echo "or"
+    echo './sshkeysetup.sh user ip'
     exit 1
 fi
 
-if [ -n "$2" ]; then
-    USER=$2
+if [ "$#" == 1 ]; then
+    USER=$(echo $1 | cut -d '@' -f 1)
+    IP=$(echo $1 | cut -d '@' -f 2)
 else
-    echo "Enter username as 2nd parameter"
-    exit 1
+    USER=$1
+    IP=$2
 fi
+
+echo "User: $USER"
+echo "IP: $IP"
 
 mkdir -p -m 700 ~/.ssh
-rm -f ~/.ssh/$(whoami)_$IP
-ssh-keygen -t rsa -f ~/.ssh/$(whoami)_$IP -N ''
+if [ ! -f ~/.ssh/id_rsa.pub ]; then
+    echo "No rsa keys found"
+    ssh-keygen -t rsa -f ~/.ssh/ -N ''
+fi
 
 eval $(ssh-agent)
 echo $SSH_AUTH_SOCK
 echo $SSH_AGENT_PID
-ssh-add ~/.ssh/$(whoami)_$IP
 ssh-keyscan -t rsa -H $IP >> ~/.ssh/known_hosts
 
-echo "cat ~/.ssh/$(whoami)_$IP.pub | ssh $USER@$IP \"mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys\"" > keycopy.sh
+echo "cat ~/.ssh/id_rsa.pub | ssh $USER@$IP \"mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys\"" > keycopy.sh
 
 chmod a+x keycopy.sh
 
 read -s -p "Enter Password: " PASS
-
-/usr/bin/expect -c '
+cat > tmp.exp << END
+#!/usr/bin/expect
+set pass [lindex $argv 0]
 spawn ./keycopy.sh
 expect "password:"
-send [lindex $argv 1]+"\n"
-interact' $PASS
-
+send "$pass\n"
+interact
+END
+chmod a+x tmp.exp
+echo "Transfering keys now"
+./tmp.exp $PASS
 rm -f keycopy.sh
-
+rm -f tmp.exp
+echo "Transfer complete."
+ssh-add 
 exit 0
